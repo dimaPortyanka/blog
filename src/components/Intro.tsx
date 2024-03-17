10e423df6d4e93af0afaa429acada77c49c72c43
Dmytro <deck@steamdeck.lan>
2024-03-16 20:02:49 -0400

    added one more rule to enforce shorter lines, added some empty components just as a reminder what i want to add

--- src/components/Intro.tsx
+++ src/components/Intro.tsx
@@ -27,10 +27,22 @@ const Intro: React.FC = () => {
                         Hey there!
                     </Heading>
                     <Text>
-                        I'm Dmytro, and I've been tinkering with code for the past decade – at least, I think it's been that long! Throughout my journey, I've dabbled in various projects, and now I'm eager to spill the beans on my experiences and thoughts.
-                        Now, let's set the record straight: I might call myself an expert, but hey, you don't have to take my word for it. What follows is just me sharing my two cents, no more, no less. Think of it as a friendly chat between us, where opinions flow and camaraderie reigns.
+                        I'm Dmytro,
+                        and I've been tinkering with code for the past decade – at least,
+                        I think it's been that long!
+                        Throughout my journey, 
+                        I've dabbled in various projects, 
+                        and now I'm eager to spill the beans on my experiences and thoughts.
+                        Now, let's set the record straight: 
+                        I might call myself an expert, 
+                        but hey, you don't have to take my word for it. 
+                        What follows is just me sharing my two cents, 
+                        no more, no less. 
+                        Think of it as a friendly chat between us, 
+                        where opinions flow and camaraderie reigns.
                         <br />
-                        So, buckle up, friend, and let's dive into the world of software engineering together!
+                        So, buckle up, friend, 
+                        and let's dive into the world of software engineering together!
                     </Text>
                 </AccordionPanel>
             </AccordionItem>
