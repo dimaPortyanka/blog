d9c3a291d2707a1561009c99649080ac9e5f38ad
Dmytro <deck@steamdeck.lan>
2024-04-21 00:20:27 -0400

    added mailchimp

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -14,17 +14,15 @@ const config: GatsbyConfig = {
         {
             resolve: '@chakra-ui/gatsby-plugin',
             options: {
-                /**
-         * @property {boolean} [resetCSS=true]
-         * if false, this plugin will not use `<CSSReset />
-         */
                 resetCSS: true,
-                /**
-         * @property {number} [portalZIndex=undefined]
-         * The z-index to apply to all portal nodes. This is useful
-         * if your app uses a lot z-index to position elements.
-         */
                 portalZIndex: undefined,
+            },
+        },
+        {
+            resolve: 'gatsby-plugin-mailchimp',
+            options: {
+                endpoint: 'https://xyz.us22.list-manage.com/subscribe/post?u=415f2a300ea7e56784ff3a934&amp;id=a03f82efda&amp;f_id=00f8c4e1f0',
+                timeout: 3500,
             },
         },
     ], 
