fa8c0cd53ad5421c0d0e799e8b7bd3c995738057
Dmytro Portianka <portyanka.d@gmail.com>
2024-11-03 09:28:57 -0500

    removed unneeded padding

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -37,10 +37,7 @@ const Layout: React.FC<LayoutProps> = ({
             )}
             <Container
                 background={'rgba(255,255,255,0.8)'}
-                marginBottom="2rem"
-                marginTop="2rem"
                 maxW="4xl"
-                paddingBottom="2rem"
             >
                 {children}
             </Container>
